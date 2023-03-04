8f5f96e1c270f95c9a445af7938ffd0dc64a4aed
Akhilesh Teja <111740608+AKHILESHTEJA7@users.noreply.github.com>
2023-03-04 12:07:01 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+function on(){
+    document.getElementById("chan").innerHTML="I AM A QUALIFIED AND PROFFESSIONAL WEB DEVELOPER";
+}
+function on1(){
+    document.getElementById("chan").innerHTML="SCHOOL: SRI CHAITANYA TECHNO SCHOOL<br>HI-SCHOOL: SRI CHAITANYA JUNIOR COLLAGE<br>UNIVERSITY: SAVEETHA SCHOOL OF ENGINEERING.";
+}
+function on2(){
+    document.getElementById("chan").innerHTML="PYTHON:🟢🟢🟢🟢🟢<br>JAVA__:🔵🔵🔵🔵<br>C_____:🟠🟠🟠🟠🟠<br>HTML__:🟡🟡🟡🟡🟡";
+}
+function on3(){
+    document.getElementById("chan").innerHTML="EDITING<br>DESIGNING<br>3D-ANIMATION";
+}
+function on4(){
+    document.getElementById("chan").innerHTML="LOCATION: 28-51,OPPSITE TO KANYAKAPRAMESWARI TEMPLE,MAIN ROAD,ADDANKI.(523201)<br>CONTACT: 9491004473";
+}
